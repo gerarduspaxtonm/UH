4a5d3150fcf11066a50a06a561e03fd4631e0e46
gerarduspaxtonm <2415624gerardus@kanisius.sch.id>
2026-05-23 21:18:01 +0700

    project page redesign

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("Website Profil Siswa TIK telah diload.");
-
-  const btnHello = document.getElementById("btnHello");
-
-  if (btnHello) {
-    btnHello.addEventListener("click", function () {
-      alert("Halo! Selamat datang di website profil saya.");
-      console.log("Tombol sapa diklik.");
-    });
-  }
+    console.log("Website Profil Siswa TIK telah diload.");
+    const btnHello = document.getElementById("btnHello");
+    if (btnHello) {
+        btnHello.addEventListener("click", function () {
+            alert("Halo! Selamat datang di website profil saya.");
+            console.log("Tombol sapa diklik.");
+        });
+    }
 });
